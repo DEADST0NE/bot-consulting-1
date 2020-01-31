4e0efd69af3c8efc3790efbe9e4b068ea65f88e3
deadstone <deadstone@list.ru>
2020-02-01 01:53:07 +0300

    Новая фотка лидера

--- components/contact_node_message.js
+++ components/contact_node_message.js
@@ -1,7 +1,11 @@
 let contactNode = function() { return `Гамид Атаев
 
+Если вашего вопроса нет в списке то обратитесь к Лидеру Ноды по контактым указанным ниже:
+
 Контакты: + 7(928)-572-51-62
+
 Telegram: @Gamidataev
+
 Instagram: https://instagram.com/gam2481?igshid=1d6ehy00x8lz9
 `; };
 let nameLidNode = function() { return `Лидер Ноды:`};
